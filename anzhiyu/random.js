6d6f86e31628c73fcfa82a7e1d8e6a5e9b3a2f43
nnnr00 <icimao0@gmail.com>
2025-03-28 22:22:50 +0800

    Site updated: 2025-03-28 22:22:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/28/hello-world/"];function toRandomPost(){
+var posts=["2025/03/28/hello-world/","2025/03/28/github部署Hexo/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
---
+++ source/_posts/github部署Hexo.md
@@ -0,0 +1,9 @@
+title: github部署Hexo
+author: John Doe
+tags: []
+categories: []
+date: 2025-03-28 21:10:00
+---
+安装hexo
+npm install hexo-cli -g
+
